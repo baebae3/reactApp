b30d4208d469a7049efd84906bd2b9199872b23d
Maksim Zakharov <Yamyamyami97@gmail.com>
2022-03-27 19:27:33 +0300

    push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
