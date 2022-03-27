07b11a6e9cca307c9698b1ab399e21619e53c65e
Maksim Zakharov <Yamyamyami97@gmail.com>
2022-03-27 19:51:58 +0300

    Lesson 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Message from './Message';
 
 function App(props) {
   return (
     <div className="App">
       <header className="App-header">
-
+      <Message txt='World!' />
       </header>
+      
     </div>
   );
 }
---
+++ src/Message.js
@@ -0,0 +1,9 @@
+
+
+const Message = (props) => {
+    return(
+        <div className="compStyle">Hello {props.txt} </div>
+    );
+}
+
+export default Message;
